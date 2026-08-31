a9658f44d94162b9e005deb7bff6bbf490b2943c
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-09-01 01:38:39 +0200

    Prepare MCG Doom 1.0.0 release

--- Assets/Mods/MCG_Doom/MCG_Doom/Scripts/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("MCG_Doom")]
-[assembly: AssemblyDescription("DOOM for Big Ambitions More Computer Games")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
