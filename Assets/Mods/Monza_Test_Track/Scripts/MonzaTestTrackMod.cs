7c336ab1b5c4310197e0a113bcfb82a130cc166a
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-09-19 11:50:19 +0200

    feat(Monza_Test_Track): load prototype on city load

--- Assets/Mods/Monza_Test_Track/Scripts/MonzaTestTrackMod.cs
+++ Assets/Mods/Monza_Test_Track/Scripts/MonzaTestTrackMod.cs
@@ -2,23 +2,37 @@
 using System;
 using System.Threading.Tasks;
 using BAModAPI;
+using UnityEngine;
 
 [assembly: RegisterModClass(typeof(MonzaTestTrack.MonzaTestTrackMod))]
 
 namespace MonzaTestTrack
 {
-    [ModEntryOnInitializationLoad]
+    [ModEntryOnCityLoad]
     public sealed class MonzaTestTrackMod : IModBigAmbitions
     {
+        private GameObject? _runtimeObject;
+
         public string[] RelativeAssetBundlePaths => Array.Empty<string>();
 
         public Task OnLoadAsync(ModContext context)
         {
+            _runtimeObject = new GameObject("MonzaTestTrack.Runtime");
+            var runtime = _runtimeObject.AddComponent<MonzaTestTrackRuntime>();
+            runtime.Initialize(context.ModRootPath, context.Logger);
             return Task.CompletedTask;
         }
 
         public Task OnUnloadAsync()
         {
+            if (_runtimeObject != null)
+            {
+                var runtime = _runtimeObject.GetComponent<MonzaTestTrackRuntime>();
+                runtime?.Shutdown();
+                UnityEngine.Object.Destroy(_runtimeObject);
+                _runtimeObject = null;
+            }
+
             return Task.CompletedTask;
         }
     }
